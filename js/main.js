8d0b1ec459adb8119dd5a2c1e21bbe8a8e6eb024
Matheus Vinicius <57428641+matheus778@users.noreply.github.com>
2022-12-06 20:19:31 -0300

    ajustes interações com o menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 let btnOpenMenu = document.querySelector('.menu-open');
 let btnCloseMenu = document.querySelector('.menu-close');
 let menu = document.querySelector('.menu-container ul');
+let links = document.querySelectorAll('.menu-container ul li a');
 
 function toaggleMenu() {
   menu.classList.toggle('show');
+}
+
+for (let link of links) {
+  link.addEventListener('click', ()=> {
+    menu.classList.remove('show');
+  });
 }
 
 btnOpenMenu.addEventListener('click', toaggleMenu);
